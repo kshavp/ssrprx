f31a79a115730f4d7cfd7e12384978f493ba4133
Keshav Priyam <keshavpriyam@gmail.com>
2024-11-26 13:08:47 +0530

    predictions working

--- src/app/predict/[name]/page.tsx
+++ src/app/predict/[name]/page.tsx
@@ -1,8 +1,32 @@
-export default function Page({params}:any) {
-    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        {params.name}
-      </div>
-    );
-  }
-  
+const getPredictedAge = async (name:string) => {
+  const res = await fetch("https://api.agify.io/?name=" + name);
+  return res.json();
+};
+const getPredictedGender = async (name:string) => {
+  const res = await fetch("https://api.genderize.io/?name=" + name);
+  return res.json();
+};
+const getPredictedCountry = async (name:string) => {
+  const res = await fetch("https://api.nationalize.io/?name=" + name);
+  return res.json();
+};
+
+interface IParams {
+  params: { name: string };
+}
+
+export default async function Page({ params }: IParams) {
+  const ageData = getPredictedAge(params.name);
+  const genderData = getPredictedGender(params.name);
+  const nationalityData = getPredictedCountry(params.name);
+
+  const [age, gender, nationality] = await Promise.all([ageData,genderData,nationalityData]);
+
+  return (
+    <div className="bg-white p-2 rounded-xl w-fit text-black">
+      <div>AGE: {age?.age}</div>
+      <div>GENDER: {gender?.gender}</div>
+      <div>COUNTRY: {nationality?.country[0]?.country_id}</div>
+    </div>
+  );
+}
